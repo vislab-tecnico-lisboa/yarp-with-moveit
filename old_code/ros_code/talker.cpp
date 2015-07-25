246164f2e86253bc99b8ce5b1cf8d5138615b0b4
Miguel Aragão <miguelaragao91@gmail.com>
2015-07-25 10:26:49 +0100

    Cleaned ROS code

--- old_code/ros_code/talker.cpp
+++ old_code/ros_code/talker.cpp
@@ -28,19 +28,6 @@ int main(int argc, char **argv)
   ros::Subscriber sub = n.subscribe("/icub_feedback", 1000, chatterCallback);
 
   ros::spin();
-  
-  /*while(true) {
-    miguel_vislab::SimpleTrajectory simpleTrajectory;
-    for(int i = 0; i < 30; i++) {
-      //ROS_INFO("I heard: [%d]", i);
-      miguel_vislab::SimpleJointTrajectoryPoint auxPoint;
-      for(int j = 0; j < 7; j++) {
-        auxPoint.positions.push_back(1.0);
-      }
-      simpleTrajectory.points.push_back(auxPoint);
-    }
-    trajectory_pub.publish(simpleTrajectory);
-  }*/
 
   return 0;
 }
--- ros_code/miguel_vislab/src/moveit_trajectory_publisher.cpp
+++ ros_code/miguel_vislab/src/moveit_trajectory_publisher.cpp
@@ -30,7 +30,7 @@ void chatterCallback(const moveit_msgs::MoveGroupActionResult& msg) {
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "listener");
+  ros::init(argc, argv, "moveit_trajectory_publisher");
 
   ros::NodeHandle n;
 
